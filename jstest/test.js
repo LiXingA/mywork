242a7723feb4847498be0d4abf6abb82097acc6f
LiXingA <55734249+LiXingA@users.noreply.github.com>
2019-10-01 14:29:45 +0800

    测试

---
+++ jstest/test.js
@@ -0,0 +1,14 @@
+function aaa(){
+    
+}
+/**
+ * @param {number}cc
+ * @param {number}dd
+ */
+function bbb(cc, dd){
+    document.getElementById("aaa");
+}
+
+var msg="hello world";
+console.log(msg);
+console.log(msg.charAt(1));
